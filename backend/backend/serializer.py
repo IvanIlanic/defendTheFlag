eb5391351df8be2555a1c27fa4d955f782fc26dc
IvanIlanic <ilanicivan@gmail.com>
2026-01-18 20:07:59 +0000

    Created Django framework, added coreheaders, jwt and rest framework, should finish serializers

---
+++ backend/backend/serializer.py
@@ -0,0 +1,4 @@
+#Serializers are here to convert from our database to json files, this will be used for data displaying
+
+from django.contrib.auth.models import User
+from rest_framework import serializers 
