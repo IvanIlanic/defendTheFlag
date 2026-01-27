de4a158f414e791447cf9d09f6232faa961a5c47
IvanIlanic <your_email@example.com>
2026-01-27 16:20:08 +0000

    Added models and token signing

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Teams(models.Model):
+    id = models.AutoField(primary_key=True) # Change this to UUID later for security reasons
+    name = models.CharField(max_length=30)
+    slang = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    # Have to add profile picture and backgroud picture
+
+    def __str__(self):
+        return self.name
--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -2,14 +2,23 @@
 # This is one weird example, have to double check this
 from django.contrib.auth.models import User
 from rest_framework import serializers 
+from .models import Teams
 # Will use ORM - Object realtional mapping
 # Serializers are transforming python data to json in this example, and other way around
 
 class UserSerializer(serializers.ModelSerializer):
-    model = User
-    fields = ["id","username", "password"]
+    class Meta:
+        model = User
+        fields = ["id","username", "password"]
+        extra_kwargs = {"password": {"write_only" : True}}
     
     def create(self, validated_data):
         print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
+    
+class TeamsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teams
+        fields = ["id","name","slang","description"]
+        extra_kwargs = {"author":{"read_only": True}}
