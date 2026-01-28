d7078d4afc75592b3256e95e9bab71cfd5ec3fbd
IvanIlanic <your_email@example.com>
2026-01-28 18:51:40 +0000

    Added routes as home, login, notFound and Register, bugs fixed in constants.js and api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,32 @@
-import react form 'react';
+import react from 'react';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Login from './pages/Login';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function Logout (){ // Used to clear the local storage from the tokens when the user logsout
+  localStorage.clear()
+  return <Navigate to="/login"/>
+}
+
+function RegisterAndLogout (){
+  localStorage.clear()
+  return <Register/>
+}
 
 function App() {
 return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ProtectedRoute> <Home /> </ProtectedRoute>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='*' element={<NotFound/>} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,23 +1,26 @@
 // Intercepter
 // Axios time for network requests
 
-import axios from "axios"
-import { ACCESS_TOKEN } from "./constants"
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
+
+const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
-
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+});
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token) {
-            config.header.Authorization = `Bearer ${token}`
-        }
-        return config
-    },
-    (error) => {
-        return Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
